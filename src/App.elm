912e4430eff85e216868f0d60c8770f0066cb69c
Rishabgh <rishabgh@Rishabghs-MacBook-Pro.local>
2020-02-18 15:47:52 +0530

    format code add styles

--- src/App.elm
+++ src/App.elm
@@ -1,30 +1,52 @@
-module App exposing (..)
+module App exposing (main)
 
 import Browser
-import Html exposing (Html, div, text, button)
+import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
 
-init: Int
-init = 0
 
-type Action = Increment | Decrement
+init : Int
+init =
+    0
+
+
+type Action
+    = Increment
+    | Decrement
+
 
 update : Action -> Int -> Int
 update action count =
     case action of
-        Increment -> count + 1
-        Decrement -> count - 1 
+        Increment ->
+            count + 1
+
+        Decrement ->
+            count - 1
+
 
 view : Int -> Html Action
-view model = 
-    div[] -- no args are taken by div
-    [
-        (button [(onClick Increment)] [(text "increment")]), -- two args are taken by button
-        (button [(onClick Decrement)] [(text "decrement")]), -- two args are taken by button
-        div [] [text (String.fromInt model)]
-    ]
+view model =
+    div
+        [ style "height" "100vh"
+        , style "display" "flex"
+        , style "align-items" "center"
+        , style "justify-content" "center"
+        ]
+        [ div
+            [ style "height" "50%"
+            , style "width" "70%"
+            , style "display" "flex"
+            , style "justify-content" "space-evenly"
+            , style "align-items" "center"
+            ]
+            [ button [ onClick Increment ] [ text "increment" ] -- two args are taken by button
+            , button [ onClick Decrement ] [ text "decrement" ] -- two args are taken by button
+            , text (String.fromInt model)
+            ]
+        ]
 
 
 main =
-    Browser.sandbox { init = init, view = view, update = update}
-    
+    Browser.sandbox { init = init, view = view, update = update }
